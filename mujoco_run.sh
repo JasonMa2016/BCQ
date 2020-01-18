9d155177e8d339ca1c7751e5265399572156b162
Jason Ma <jasonma@Jasons-MBP-6.fios-router.home>
2020-01-18 14:38:13 -0500

    scripted

---
+++ mujoco_run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for env in Reacher-v2 Hopper-v2 HalfCheetah-v2 Walker2d-v2 Ant-v2 Humanoid-v2
+do
+    python3 train_bc.py --env_name $env --num_trajs 5
+    python3 train_drbcq_traj.py --env_name $env --num_trajs 5
+done
