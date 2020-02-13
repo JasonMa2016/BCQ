bdd8a42e3ef25d533564323acd887229019b0f96
Ubuntu <ubuntu@ip-172-31-40-170.ec2.internal>
2020-02-13 04:19:49 +0000

    done

--- mujoco_run.sh
+++ mujoco_run.sh
@@ -2,8 +2,8 @@
 
 for env in Hopper-v2
 do
-    python3 train_bc.py --env_name $env --num_trajs 5 --seed 0
-    python3 train_bc.py --env_name $env --num_trajs 5 --seed 0 --good
+    python3 train_bc.py --env_name $env --num_trajs 5 --seed 0 --ensemble
+    python3 train_bc.py --env_name $env --num_trajs 5 --seed 0 --good --ensemble
     python3 train_drbcq_traj.py --env_name $env --num_trajs 5 --seed 0
     python3 train_drbcq_traj.py --env_name $env --num_trajs 5 --seed 0 --good
     python3 train_bcq_traj.py --env_name $env --num_trajs 5 --seed 0
