4776be6a80037adb09b86695ce59f0b2958cbb8a
Jason Ma <jasonma@dhcp-10-250-104-166.harvard.edu>
2020-02-17 00:56:57 -0500

    ok

--- mujoco_run.sh
+++ mujoco_run.sh
@@ -9,9 +9,9 @@ for env in Hopper-v2
 do
     for seed in 0 1 2
     do
-        python3 train_drbcq_traj.py --env_name $env --num_trajs 5 --seed seed
-#        python3 train_drbcq_traj.py --env_name $env --num_trajs 5 --seed seed --good
-#        python3 train_bcq_traj.py --env_name $env --num_trajs 5 --seed seed
-#        python3 train_bcq_traj.py --env_name $env --num_trajs 5 --seed seed --good
+        python3 train_drbcq_traj.py --env_name $env --num_trajs 5 --seed $seed
+#        python3 train_drbcq_traj.py --env_name $env --num_trajs 5 --seed $seed --good
+#        python3 train_bcq_traj.py --env_name $env --num_trajs 5 --seed $seed
+#        python3 train_bcq_traj.py --env_name $env --num_trajs 5 --seed $seed --good
     done
 done
