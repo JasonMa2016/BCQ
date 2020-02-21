b5f87b115d951fdbcc4f0b69030259a0f5e2fd9d
Jason Ma <jasonma@dhcp-10-250-102-73.harvard.edu>
2020-02-21 15:02:39 -0500

    experts

--- mujoco_train_experts.sh
+++ mujoco_train_experts.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-for env in Hopper-v2
+for env in Reacher-v2 Hopper-v2 HalfCheetah-v2 Walker2d-v2 Ant-v2 Humanoid-v2
 do
-    python3 train_expert.py --env-name $env --start-timesteps 1000
-done
-for env in Reacher-v2 HalfCheetah-v2 Walker2d-v2 Ant-v2 Humanoid-v2
-do
-    python3 train_expert.py --env-name $env
+    python3 train_expert.py --env_name $env
 done
