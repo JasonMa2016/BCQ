22b7b9b37cf38cc00a6d018e0576316284954f82
Jason Ma <jasonma@dhcp-10-250-251-16.harvard.edu>
2020-02-29 09:58:34 -0500

    humanoid

--- scripts/mujoco_gail.sh
+++ scripts/mujoco_gail.sh
@@ -4,7 +4,7 @@ ENV="Humanoid-v2"
 
 for num_traj in 1 3 5
 do
-    for seed in 1 2 3 4
+    for seed in 0
     do
         python3 train_gail_ppo_expert.py --env_name $ENV --num_trajs $num_traj --seed $seed
         python3 train_gail_ppo_expert.py --env_name $ENV --num_trajs $num_traj --seed $seed --good
