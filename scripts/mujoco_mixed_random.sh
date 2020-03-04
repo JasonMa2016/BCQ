e60a17b7b83627466b9a250fb81b99ed58f0bd9d
Jason Ma <jasonma@dhcp-10-250-251-16.harvard.edu>
2020-03-04 14:02:20 -0500

    done

--- scripts/mujoco_mixed_random.sh
+++ scripts/mujoco_mixed_random.sh
@@ -4,6 +4,6 @@ ENV="Walker2d-v2"
 
 for seed in 0 1 2 3 4
 do
-    python3 train_drbcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed --good --random
+    python3 train_bcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed
     python3 train_drbcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed --random
 done
--- scripts/mujoco_one_environment.sh
+++ scripts/mujoco_one_environment.sh
@@ -8,9 +8,10 @@ python3 train_bc.py --env_name $ENV --num_trajs 5 --seed 0 --ensemble
 for seed in 0 1 2 3 4
 do
     python3 train_bcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed --good
+    python3 train_bcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed
     python3 train_drbcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed --good
     python3 train_drbcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed
     python3 train_drbcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed --good --random
-        python3 train_drbcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed --random
+    python3 train_drbcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed --random
 
 done
