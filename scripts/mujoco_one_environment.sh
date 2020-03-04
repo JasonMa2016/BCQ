c1ba69743b22750c5fd3f691df41ba68ded35703
Ubuntu <ubuntu@ip-172-31-40-170.ec2.internal>
2020-03-04 17:51:38 +0000

    ok

--- scripts/mujoco_one_environment.sh
+++ scripts/mujoco_one_environment.sh
@@ -5,10 +5,10 @@ ENV="Walker2d-v2"
 #python3 train_bc.py --env_name $ENV --num_trajs 5 --seed 0 --good --ensemble
 #python3 train_bc.py --env_name $ENV --num_trajs 5 --seed 0 --ensemble
 
-for seed in 0
+for seed in 3 4  
 do
     python3 train_bcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed --good
     python3 train_drbcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed --good
-    python3 train_drbcq_traj.py --env_name $NEV --num_trajs 5 --seed $seed
-    python3 train_drbcq_traj.py --env_name $NEV --num_trajs 5 --seed $seed --good --random
+    python3 train_drbcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed
+    python3 train_drbcq_traj.py --env_name $ENV --num_trajs 5 --seed $seed --good --random
 done
