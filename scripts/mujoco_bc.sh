6095f969c5549f85add5cd13209f3f994e815fe1
Jason Ma <jasonma@dhcp-10-250-251-16.harvard.edu>
2020-03-05 15:57:52 -0500

    BC

--- scripts/mujoco_bc.sh
+++ scripts/mujoco_bc.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-for env in Hopper-v2 Walker2d-v2 Humanoid-v2
-do
-    python3 train_bc.py --env_name $ENV --num_trajs 5 --seed 0 --good --ensemble
-    python3 train_bc.py --env_name $ENV --num_trajs 5 --seed 0 --ensemble
-done
+ENV="Hopper-v2"
+
+python3 train_bc.py --env_name $ENV --num_trajs 5 --seed 0 --good --ensemble
+python3 train_bc.py --env_name $ENV --num_trajs 5 --seed 0 --ensemble
 
