e157c436717a6111e27bf8560966385eb4d5e4e8
Jason Ma <jasonma@dhcp-10-250-251-16.harvard.edu>
2020-03-07 12:01:39 -0500

    done

--- scripts/mujoco_bc.sh
+++ scripts/mujoco_bc.sh
@@ -6,8 +6,8 @@ for env in Hopper-v2 Walker2d-v2
 do
     for seed in 0 1 2
     do
-        python3 train_bc.py --env_name env --num_trajs $TRAJ --seed $seed --good
-        python3 train_bc.py --env_name env --num_trajs $TRAJ --seed $seed
+        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --good
+        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed
     done
 done
 
