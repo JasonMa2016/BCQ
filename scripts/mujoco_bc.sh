0ab0fd59775afb084ef4ccfd7a1bebe8bcc9f777
Jason Ma <jasonma@Jasons-MacBook-Pro-6.local>
2020-03-08 15:26:09 -0400

    new exps

--- scripts/mujoco_bc.sh
+++ scripts/mujoco_bc.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-TRAJ=5
+TRAJ=1
 
 for env in Hopper-v2 Walker2d-v2
 do
-    for seed in 0 1 2
+    for seed in 0
     do
         python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --good
-        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed
+        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --mixed
+        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --imperfect
     done
 done
 
