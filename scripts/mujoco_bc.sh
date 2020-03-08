0027cffa81482dce21d02f39bc11c92a0f5e25da
Jason Ma <jasonma@Jasons-MacBook-Pro-6.local>
2020-03-08 15:27:32 -0400

    ok

--- scripts/mujoco_bc.sh
+++ scripts/mujoco_bc.sh
@@ -6,9 +6,9 @@ for env in Hopper-v2 Walker2d-v2
 do
     for seed in 0
     do
-        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --good
-        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --mixed
-        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --imperfect
+        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --type "good"
+        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --type "mixed"
+        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --type "imperfect"
     done
 done
 
