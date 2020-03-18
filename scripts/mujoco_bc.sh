b1b762a47581f9ea395d6a4c5d65688e060abc28
Jason Ma <jasonma@Jasons-MBP-6.fios-router.home>
2020-03-18 16:15:12 -0400

    ok

--- scripts/mujoco_bc.sh
+++ scripts/mujoco_bc.sh
@@ -4,8 +4,8 @@ TRAJ=1
 ENV='Walker2d-v2'
 for seed in 1 2 3 4
 do
-#        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --type "good"
-#        python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --type "mixed"
+    python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --type "good"
+    python3 train_bc.py --env_name $env --num_trajs $TRAJ --seed $seed --type "mixed"
     python3 train_bc.py --env_name $ENV --num_trajs $TRAJ --seed $seed --type "imperfect"
 done
 
