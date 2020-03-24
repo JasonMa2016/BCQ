5f404ca52a5557a2090dd593b6b4167c46a59e27
Jason Ma <jasonma@Jasons-MBP-6.fios-router.home>
2020-03-23 22:30:00 -0400

    fixed scripts

--- scripts/mujoco_drbcq.sh
+++ scripts/mujoco_drbcq.sh
@@ -6,7 +6,7 @@ for env in Walker2d-v2
 do
     for seed in 0 1 2 3 4
     do
-        python3 train_drbcq_traj.py --env_name $env --num_trajs $TRAJ --seed $seed --good
-        python3 train_drbcq_traj.py --env_name $env --num_trajs $TRAJ --seed $seed
+        python3 train_drbcq_traj.py --env_name $env --num_trajs $TRAJ --seed $seed --type "good"
+        python3 train_drbcq_traj.py --env_name $env --num_trajs $TRAJ --seed $seed --type "mixed"
     done
 done
