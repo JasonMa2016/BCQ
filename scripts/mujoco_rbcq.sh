a5a85b5cd1b7039a5e4943b06fb06148d8f57ca5
Jason Ma <jasonma@Jasons-MBP-6.fios-router.home>
2020-03-25 21:17:34 -0400

    random

---
+++ scripts/mujoco_rbcq.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ENV='Hopper-v2'
+for traj in 1 3 5
+do
+for seed in 0 1 2 3 4
+do
+    python3 train_drbcq_traj.py --env_name $ENV --num_trajs $traj --seed $seed --type "good" --random
+    python3 train_drbcq_traj.py --env_name $ENV --num_trajs $traj --seed $seed --type "mixed" --random
+done
+done
