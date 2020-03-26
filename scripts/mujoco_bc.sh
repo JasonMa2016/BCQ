4a3c3329afdf5ab8f7e6b874701971cebe962568
Jason Ma <jasonma@Jasons-MBP-6.fios-router.home>
2020-03-25 21:20:28 -0400

    ok

--- scripts/mujoco_bc.sh
+++ scripts/mujoco_bc.sh
@@ -5,8 +5,8 @@ for traj in 1 3 5
 do
 for seed in 0 1 2 3 4
     do
-        python3 train_bc.py --env_name $ENV --num_trajs $TRAJ --seed $seed --type "good"
-        python3 train_bc.py --env_name $ENV --num_trajs $TRAJ --seed $seed --type "mixed"
+        python3 train_bc.py --env_name $ENV --num_trajs $traj --seed $seed --type "good"
+        python3 train_bc.py --env_name $ENV --num_trajs $traj --seed $seed --type "mixed"
     done
 done
 
